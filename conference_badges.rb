b1e8f298b4cb0a28c2ef7bca3617722d07967556
JedwardMook <jedwardmook@yahoo.com>
2022-06-01 16:24:28 -0500

    commit tree 353d5828ec40892aac074853152bf349215e3125

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+require 'pry'
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms array
+    array.each_with_index.map do |name, index| 
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer array
+   greeting_array = batch_badge_creator(array)
+   greeting_array.map {|greeting| puts greeting}
+   assignment_array = assign_rooms(array)
+   assignment_array.map {|assignment| puts assignment}
+end
+
+
